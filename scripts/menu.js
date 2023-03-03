91f1d8cc20216012b90d5c1f8f770207ec39b23c
Dustin <ddemers1995@hotmail.com>
2023-03-02 20:44:52 -0800

    huge update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,7 @@
-const hamburger = document.querySelector('.hamburger');
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector('.mobile-nav');
 
-hamburger.addEventListener('click', function() {
-    this.classList.toggle('is-active');
+menu_btn.addEventListener("click", function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle("is-active");
 });
